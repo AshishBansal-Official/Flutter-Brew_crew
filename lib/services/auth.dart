4e7b42ded2f651630e3343523f172db3e55d379d
ashish <ash23581221@gmail.com>
2020-02-24 00:35:54 +0530

    Firebase Auth Service

---
+++ lib/services/auth.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in Annonymously
+  Future signinAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Sign in with Email & Password
+
+  // Register with Email & Password
+
+  // Sign out 
+
+
+}
