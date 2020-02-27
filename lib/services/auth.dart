f2305ee8d73e48fb55beff22d03496658fc6291b
ashish <ash23581221@gmail.com>
2020-02-27 15:41:11 +0530

    Firestore User Records

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:brew_crew/models/user.dart';
 
@@ -44,6 +45,9 @@ class AuthService {
     try {
       AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       FirebaseUser user = result.user;
+
+      // Create a new document for the user with the uid
+      await DatabaseService(uid: user.uid).updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
